7e3c72a91ec0bcbd4c2f793d2d046727fea97bb1
rociorequena <rreqgon2108@g.educaand.es>
2025-11-05 12:21:35 +0100

    cambios

--- Docker/Caronte/dockerfiles/base/admin/start.sh
+++ Docker/Caronte/dockerfiles/base/admin/start.sh
@@ -1,4 +1,15 @@
 #!/bin/bash
 
+newUser(){
+useradd -rm -d /home/rocio -s /bin/bash rocio
+echo "rocio:1234" | chpasswd 
+echo "Bienvenida Rocio!"> /home/rocio/bienvenida.txt
+}
+
+main(){
+    newUser
 tail -f /dev/null
 ## script's que se encargan de configurar el contenedor y la imagen.
+}
+
+main

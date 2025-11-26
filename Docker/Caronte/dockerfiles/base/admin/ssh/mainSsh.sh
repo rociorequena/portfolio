6e6fee50d3511be63fb7eb7260ab02e13b18db97
rociorequena <rreqgon2108@g.educaand.es>
2025-11-26 10:22:34 +0100

    .

--- Docker/Caronte/dockerfiles/base/admin/ssh/mainSsh.sh
+++ Docker/Caronte/dockerfiles/base/admin/ssh/mainSsh.sh
@@ -4,5 +4,9 @@ set -e
 
 make_ssh(){
     sed -i 's/#Port 22/Port 2345/' /etc/ssh/sshd_config
-    sed -i 's/#PermitRootLogin prohibit-password/PermitRootLogin yes/' /
+    sed -i 's/#PermitRootLogin prohibit-password/PermitRootLogin yes/' /etc/ssh/sshd_config
+
+    mkdir -p /home/${USUARIO}/.ssh
+    cp /root/common/id_ed25519.pub /home/${USUARIO}/.ssh/
+    
 }
--- Docker/Caronte/dockerfiles/base/admin/start.sh
+++ Docker/Caronte/dockerfiles/base/admin/start.sh
@@ -1,25 +1,3 @@
-config_ssh(){
-    sed -i 's/#PermitRootLogin prohibit-password/PermitRootLogin yes/' /etc/ssh/sshd_config
-    sed -i 's/#Port 22/Port 22/' /etc/ssh/sshd_config
-    if [ ! -d /home/${USUARIO}/.ssh ]; 
-    then
-        mkdir /home/${USUARIO}/.ssh
-        cat /root/datos/id_rsa.pub >> /home/${USUARIO}/.ssh/authorized_keys
-
-    fi
-    # /etc/init.d/ssh start &
-    #exec /usr/sbin/sshd -D & #dejar el ssh en background (2plano)
-}
-
-#rocio ALL=(ALL:ALL) ALL
-config_sudoer(){
-    if [ -f /etc/sudoers]
-    then
-    #comprobar que el ${USUARIO} no existe
-        echo "${USUARIO} ALL=(ALL:ALL) ALL" >> /etc/sudoers
-    fi
-}
-
 #!/bin/bash
 # carga las variables de entono pasadas desde el D.Compose
 set -e
@@ -35,12 +13,16 @@ main() {
     # gestion del ssh ---> gestSsh.sh
     # ...
     touch /root/logs/informe.log
-    newUser
-    # if [ "$?" -eq 0 ]
-    # then
-    #     make_ssh
-    # fi
+    resuser=newUser
+    if [ "$resuser" -eq 0 ]
+    then
+        make_ssh
+    fi
 
+    if [ "$?" -eq 0 ]
+    then
+        make_sudo
+    fi
     # encargada de dejar este contendor vivo en BGround
     tail -f /dev/null
     ## script's que se encargar de configurar el imagen/contenedor
